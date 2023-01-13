c8bffba1fe23d56ed2e86d5a249edcb1d1dd2976
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-01-13 17:19:38 -0500

    addeed all types in ergast api

---
+++ src/utils/types.ts
@@ -0,0 +1,94 @@
+export type Drivers = Driver[];
+
+export interface Driver {
+    id: string;
+    code: string;
+    number?: number;
+    firstName: string;
+    lastName: string;
+    birthday: string;
+    nationality: string;
+}
+
+export type Constructors = Constructor[];
+
+export interface Constructor {
+    id: string;
+    name: string;
+    nationality: string;
+}
+
+export interface Circuit {
+    id: string;
+    name: string;
+    location: Location;
+}
+
+export type Results = Result[];
+
+export interface Result {
+    season: number;
+    location: Location
+}
+
+export type QualifyingResults = Qualifying[];
+
+export interface Qualifying {
+    season: number;
+    location: Location;
+}
+
+export type RacesSchedule = RaceSchedule[];
+
+export interface RaceSchedule {
+    season: number;
+    round: number;
+    name: string;
+    circuit: Circuit;
+    date: Date;
+}
+
+export type DriverStandings = DriverStanding[];
+
+export interface StandingList<T> {
+    season: number;
+    number: number;
+    standing: T;
+}
+
+export interface DriverStanding {
+    position: number;
+    points: number;
+    wins: number;
+    constructor: Constructor;
+}
+
+export type FinishingStatus = FinishingStatus[];
+
+export interface PitStop {
+    season: number;
+    location: Location;
+}
+
+export type LapTimes = LapTime[];
+
+export interface LapTime {
+    season: number;
+    lapNumber: number;
+    location: Location;
+}
+
+export type PitStops = PitStop[];
+
+export interface PitStop {
+    id: number;
+    season: number;
+    location: Location
+}
+
+export interface Location {
+    latitude: number;
+    longitude: number;
+    country: string;
+    city: string;
+}
