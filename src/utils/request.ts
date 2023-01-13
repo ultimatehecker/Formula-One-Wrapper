c0c50eadd0d4cedebebb56d429f92115546ceae7
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-01-13 18:00:52 -0500

    fixed compliling errors

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import to from 'await-to-js'
-import Parser from '@parsers/parser'
+import Parser from '../parsers/parser'
 
 const baseUrl = 'http://ergast.com/api/f1';
 
@@ -11,7 +11,7 @@ export const makeRequest = async <T>(url: string, parser: Parser, ...extraParams
         throw err;
     }
 
-    return parser.parse.apply(null, [cleanResponses(response?.data), ...extraParams]);
+    return parser.parse.apply(null, [cleanResponses(response?.data)]); // add ...extraParams later
 };
 
 const cleanResponses = (data: any): any => {
