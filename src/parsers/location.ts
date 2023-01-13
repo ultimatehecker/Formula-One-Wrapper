ea2627f64635020166f40c5b2e687aeb27692cf5
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-01-13 17:42:45 -0500

    added location parser

---
+++ src/parsers/location.ts
@@ -0,0 +1,29 @@
+import Parser from '@parsers/parser';
+import { Location } from '@utils/types'
+
+export default class LocationParser extends Parser {
+
+    private static instance: LocationParser;
+
+    public static getInstance() {
+        if(this.instance) {
+            this.instance = new LocationParser();
+        }
+
+        return this.instance
+    }
+
+    public parse(data: any): any {
+        const { lat, long, locality: city, country } = data;
+
+        const location: Location = {
+            latitude: parseFloat(lat),
+            longtitude: parseFloat(long),
+            city, 
+            country,
+        };
+
+        return location
+    }
+    
+}
