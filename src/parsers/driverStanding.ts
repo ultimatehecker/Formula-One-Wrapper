bcbdd8ac414f9cb73dafdac09aff9e7e4e33681b
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-01-14 13:01:03 -0500

    addeed driverStanding parser

---
+++ src/parsers/driverStanding.ts
@@ -0,0 +1,35 @@
+import { DriverStanding } from '../utils/types';
+import Parser from './parser';
+import DriverParser from './driver';
+import ConstructorsParser from './constructors';
+
+export default class DriverStandingParser extends Parser {
+
+    private static instance: DriverStandingParser;
+
+    public static getInstance() {
+        if(!this.instance) {
+            this.instance = new DriverStandingParser();
+        }
+
+        return this.instance
+    }
+
+    public parse(data: any): any {
+        const constructorsParser = ConstructorsParser.getInstance()
+        const driverParser = DriverParser.getInstance();
+
+        const { position, points, wins, Driver: driverData, Constructors: constructors } = data;
+        const driverStanding: DriverStanding = {
+            position: parseInt(position, 10),
+            points: parseFloat(points),
+            wins: parseInt(wins, 10),
+            driver: driverParser.parse(driverData),
+            constructors: constructorsParser.parse({
+                Constructors: [...constructors],
+            }),
+        };
+
+        return driverStanding
+    }
+}
