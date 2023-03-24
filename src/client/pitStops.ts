3b78e8b438e7ba0ef7642f4f8f22562eb9306706
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-24 09:32:14 -0400

    added pitStop return type

---
+++ src/client/pitStops.ts
@@ -0,0 +1,36 @@
+import getRequest from "../utils/request";
+import PitStop from "../config/pitStops/pitStop";
+import PitStopList from "../config/pitStops/pitStopList";
+import config from "../utils/config";
+
+export default class PitStops {
+    getPitStops(season: string, round: string, callback: any, pitStop: any, responsesValidator: any) {
+        let url = config.baseUrl + season + "/" + round + "/pitstops/" + pitStop + ".json";
+        getRequest(url, 0, config.defaultResponseRows, function(err: any, response: any) {
+            if (err) {
+                callback(err);
+            }
+            else if (responsesValidator.responseWithoutRaces(response)) {
+                callback(new Error('Invalid season/round/pitStop.'));
+            }
+            else {
+                callback(null, new PitStopList(response["MRData"]["RaceTable"]["Races"][0]["PitStops"]));
+            }
+        });
+    }
+
+    getDriverPitStops(season: string, round: string, driverId: string, callback: any, pitStop: any, responsesValidator: any)  {
+        let url = config.baseUrl + season + "/" + round + "/drivers/"+ driverId + "/pitstops/" + pitStop + ".json";
+        getRequest(url, 0, config.defaultResponseRows, function(err: any, response: any) {
+            if (err) {
+                callback(err);
+            }
+            else if (responsesValidator.responseWithoutRaces(response)) {
+                callback(new Error('Invalid season/round/pitStop/driverId.'));
+            }
+            else {
+                callback(null, new PitStop(response["MRData"]["RaceTable"]["Races"][0]["PitStops"][0]));
+            }
+        });
+    }
+}
