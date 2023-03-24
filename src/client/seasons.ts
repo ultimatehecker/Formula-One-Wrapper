f0ff94e9eeea6f5d0d2c484fd3dd527ae22f3702
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-24 16:51:46 -0400

    added season return type

---
+++ src/client/seasons.ts
@@ -0,0 +1,20 @@
+import getRequest from "../utils/request";
+import Season from "../config/seasons/season";
+import config from "../utils/config";
+
+export default class Seasons {
+    getSeasons(season: any, callback: any, responsesValidator: any) {
+        let url = config.baseUrl + season + ".json";
+        getRequest(url, 0, config.defaultResponseRows, function(err: any, response: any) {
+            if (err) {
+                callback(err);
+            }
+            else if (responsesValidator.responseWithoutRaces(response)) {
+                callback(new Error('Invalid season.'));
+            }
+            else {
+                callback(null, new Season(response["MRData"]["RaceTable"]["Races"]));
+            }
+        });
+    }
+}
