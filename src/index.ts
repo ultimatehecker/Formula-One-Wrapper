5bee5f814f9ec8af24c56bba7652ff1f5369e26f
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-25 11:41:04 -0400

    built the f1Client

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,162 @@ import LapTimes from "./client/lapTimes";
 import PitStops from "./client/pitStops";
 import ResponsesValidator from "./client/responsesValidator";
 
-function ErgastClient() {
-    let ResponsesValidator = new ResponsesValidator();
-    let seasons = new Seasons(responsesValidator);
-    let races = new Races(responsesValidator);
-    let raceResults = new RaceResults(responsesValidator);
-    let qualifyingResults = new QualifyingResults(responsesValidator);
-    let standings = new Standings(responsesValidator);
-    let drivers = new Drivers(responsesValidator);
-    let constructors = new Constructors(responsesValidator);
-    let circuits = new Circuits(responsesValidator);
-    let finishingStatuses = new FinishingStatuses(responsesValidator);
-    let lapTimes = new LapTimes(responsesValidator);
-    let pitStops = new PitStops(responsesValidator);
+export default class ErgastClient {
+    constructor() {
+        this.responsesValidator = new ResponsesValidator();
+        this.seasons = new Seasons(this.responsesValidator);
+        this.races = new Races(this.responsesValidator);
+        this.raceResults = new RaceResults(this.responsesValidator);
+        this.qualifyingResults = new QualifyingResults(this.responsesValidator);
+        this.standings = new Standings(this.responsesValidator);
+        this.drivers = new Drivers(this.responsesValidator);
+        this.constructors = new Constructors(this.responsesValidator);
+        this.circuits = new Circuits(this.responsesValidator);
+        this.finishingStatuses = new FinishingStatuses(this.responsesValidator);
+        this.lapTimes = new LapTimes(this.responsesValidator);
+        this.pitStops = new PitStops(this.responsesValidator);
+    }
+
+    responsesValidator: ResponsesValidator
+    seasons: Seasons
+    races: Races
+    raceResults: RaceResults
+    qualifyingResults: QualifyingResults
+    standings: Standings
+    drivers: Drivers
+    constructors: Constructors
+    circuits: Circuits
+    finishingStatuses: FinishingStatuses
+    lapTimes: LapTimes
+    pitStops: PitStops
+
+    getSeason(year: any, callback: any) {
+        this.seasons.getSeason(year, this.responsesValidator, function(err: any, season: any) {
+            callback(err, season);
+        });
+    }
+
+    getRace(season: any, round: any, callback: any) {
+        this.races.getRace(season, round, this.responsesValidator, function(err: any, race: any) {
+            callback(err, race);
+        });
+    }
+
+    getLastRace(callback: any) {
+        this.races.getLastRace(this.responsesValidator, function(err: any, race: any) {
+            callback(err, race)
+        });
+    }
+
+    getRaceResults(season: any, round: any, callback: any) {
+        this.raceResults.getRaceResults(season, round, this.responsesValidator, function(err: any, raceResults: any) {
+            callback(err, raceResults);
+        });
+    }
+
+    getQualifyingResults(season: any, round: any, callback: any) {
+        this.qualifyingResults.getQualifyingResults(season, round, this.responsesValidator, function(err: any, qualifyingResults: any) {
+            callback(err, qualifyingResults);
+        });
+    }
+
+    getDriverStandings(season: any, callback: any) {
+        this.standings.getDriverStandings(season, this.responsesValidator, function(err: any, driverStandings: any) {
+            callback(err, driverStandings);
+        });
+    }
+
+    getDriverStandingAfterRound(season: any, round: any, callback: any) {
+        this.standings.getDriverStandingAfterRound(season, round, this.responsesValidator, function(err: any, driverStandings: any) {
+            callback(err, driverStandings);
+        });
+    }
+
+    getConstructorStandings(season: any, callback: any) {
+        this.standings.getConstructorStandings(season, this.responsesValidator, function(err: any, constructorStandings: any) {
+            callback(err, constructorStandings);
+        });
+    }
+
+    getConstructorStandingAfterRound(season: any, round: any, callback: any) {
+        this.standings.getConstructorStandingAfterRound(season, round, this.responsesValidator, function(err: any, constructorStandings: any) {
+            callback(err, constructorStandings);
+        });
+    }
+
+    getDriver(driverId: any, callback: any) {
+        this.drivers.getDriver(driverId, this.responsesValidator, function(err: any, driver: any) {
+            callback(err, driver);
+        });
+    }
+
+    getDrivers(year: any, callback: any) {
+        this.drivers.getDrivers(year, this.responsesValidator, function(err: any, drivers: any) {
+            callback(err, drivers);
+        });
+    }
+
+    getConstructor(constructorId: any, callback: any) {
+        this.constructors.getConstructor(constructorId, this.responsesValidator, function(err: any, constructor: any) {
+            callback(err, constructor);
+        });
+    }
+
+    getConstructors(year: any, callback: any) {
+        this.constructors.getConstructors(year, this.responsesValidator, function(err: any, constructors: any) {
+            callback(err, constructors);
+        });
+    }
+
+    getCircuit(circuitId: any, round: any, callback: any) {
+        this.circuits.getCircuit(circuitId, round, this.responsesValidator, function(err: any, circuit: any) {
+            callback(err, circuit);
+        });
+    }
+
+    getCircuits(year: any, callback: any) {
+        this.circuits.getCircuits(year, this.responsesValidator, function(err: any, circuits: any) {
+            callback(err, circuits);
+        });
+    }
+
+    getFinishingStatuses(season: any, round: any, callback: any) {
+        this.finishingStatuses.getFinishingStatuses(season, round, this.responsesValidator, function(err: any, finishingStatuses: any) {
+            callback(err, finishingStatuses);
+        });
+    }
+
+    getYearFinishingStatuses(season: any, callback: any) {
+        this.finishingStatuses.getYearFinishingStatuses(season, this.responsesValidator, function(err: any, finishingStatuses: any) {
+            callback(err, finishingStatuses);
+        });
+    }
+
+    getLap(season: any, round: any, lapNumber: any, callback: any) {
+        this.lapTimes.getLap(season, round, lapNumber, this.responsesValidator, function(err: any, lap: any) {
+            callback(err, lap);
+        });
+    }
+
+    /*
+
+    getLaps(season: any, round: any, callback: any) {
+        this.lapTimes.getLaps(season, round, this.responsesValidator, function(err: any, laps: any) {
+            callback(err, laps);
+        });
+    }
+
+    */
+
+    getPitStop(season: any, round: any, pitStopNumber: any, callback: any) {
+        this.pitStops.getPitStop(season, round, pitStopNumber, this.responsesValidator, function(err: any, pitStop: any) {
+            callback(err, pitStop);
+        });
+    }
+
+    getDriverPitStops(season: any, round: any, driverId: any, pitStopNumber: any,  callback: any) {
+        this.pitStops.getDriverPitStops(season, round, driverId, pitStopNumber, this.responsesValidator, function(err: any, pitStops: any) {
+            callback(err, pitStops);
+        });
+    }
 }
