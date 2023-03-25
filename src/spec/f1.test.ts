cf87b653e8562cf7643ae67894be7c384ddb297d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-25 11:40:53 -0400

    example test script

---
+++ src/spec/f1.test.ts
@@ -0,0 +1,15 @@
+const ErgastClient = require('../src/index');
+
+let ergastClient = new ErgastClient();
+
+describe("Testing the ErgastClient responses", () => {
+    it("Returns a that contains the Spanish Grand Prix, celebrated in Montmeló, Spain on 2014-05-11", (done) => {
+        ergastClient.getSeason(2014, (err: any, season: any) => {
+            let race = season.getRaceByRound(5);
+            expect(race.raceName).toEqual("Spanish Grand Prix");
+            expect(race.circuit.location.country).toEqual("Spain");
+            expect(race.date).toEqual("2014-05-11");
+            done();
+        });
+    });
+});
