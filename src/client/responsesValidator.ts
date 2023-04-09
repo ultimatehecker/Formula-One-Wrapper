3da12030a7d127e58ee4faa785fcd197acf43ad5
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-04-09 17:07:56 -0400

    fixed MRData not accessing the data property

--- src/client/responsesValidator.ts
+++ src/client/responsesValidator.ts
@@ -1,25 +1,27 @@
 export default class ResponsesValidator {
+
     responseWithoutRaces(callResponse: any) {
-        return callResponse["MRData"]["RaceTable"]["Races"].length === 0;
+        console.log(callResponse);
+        return callResponse.data.MRData.RaceTable.Races.length === 0;
     }
 
     responseWithoutCircuits(callResponse: any) {
-        return callResponse["MRData"]["CircuitTable"]["Circuits"].length === 0;
+        return callResponse.data.MRData.CircuitTable.Circuits.length === 0;
     }
 
     responseWithoutConstructors(callResponse: any) {
-        return callResponse["MRData"]["ConstructorTable"]["Constructors"].length === 0;
+        return callResponse.data.MRData.ConstructorTable.Constructors.length === 0;
     }
 
     responseWithoutDrivers(callResponse: any) {
-        return callResponse["MRData"]["DriverTable"]["Drivers"].length === 0;
+        return callResponse.data.MRData.DriverTable.Drivers.length === 0;
     }
 
     responseWithoutStatuses(callResponse: any) {
-        return callResponse["MRData"]["StatusTable"]["Status"].length === 0;
+        return callResponse.data.MRData.StatusTable.Status.length === 0;
     }
 
     responseWithoutStandingsLists(callResponse: any) {
-        return callResponse["MRData"]["StandingsTable"]["StandingsLists"].length === 0;
+        return callResponse.data.MRData.StandingsTable.StandingsLists.length === 0;
     }
 }
