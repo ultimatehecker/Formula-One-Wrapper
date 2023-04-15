e378c15e77da039cec216759bc1bab0f96602b32
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-04-15 11:32:49 -0400

    logging response for debugging purposes

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 function getRequest(url: any, offset: any, limit: any, callback: any) {
     axios.get(url + "?offset=" + offset + "&limit=" + limit).then(function(response: any) {
+        console.log(response);
         if (response instanceof Error) {
             callback(response, null);
         }
