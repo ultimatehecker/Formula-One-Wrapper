9884a44e557693f5ba7c059b92d42e019b620bd0
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-04-15 13:42:37 -0400

    feat(create-turbo): apply official-starter transform

--- src/spec/f1.test.ts
+++ src/spec/f1.test.ts
@@ -2,8 +2,8 @@ import ErgastClient from '../api';
 
 let ergastClient = new ErgastClient();
 
-describe("Testing the ErgastClient responses", () => {
-    it("Returns a that contains the Spanish Grand Prix, celebrated in Montmeló, Spain on 2014-05-11", (done) => {
+describe("Testing the ErgastClient responses", function() {
+    it("Returns a that contains the Spanish Grand Prix, celebrated in Montmeló, Spain on 2014-05-11", (done: any) => {
         ergastClient.getSeason(2014, (err: any, season: any) => {
             let race = season.getRaceByRound(5);
             expect(race.raceName).toEqual("Spanish Grand Prix");
@@ -11,5 +11,29 @@ describe("Testing the ErgastClient responses", () => {
             expect(race.date).toEqual("2014-05-11");
             done();
         });
-    });
+    }, 10000);
+
+    it("Should return an error because Formula 1 didn't exist in 1900", (done: any) => {
+        ergastClient.getSeason(1900, (err: any, season: any) => {
+            expect(err).toBeDefined();
+            expect(season).toBeUndefined();
+            done();
+        });
+    }, 10000);
+
+    it("Returns the Chinese Grand Prix which was held on 2014-04-20", (done: any) => {
+        ergastClient.getRace(2014, 4, function(err: any, race: any) {
+            expect(race.date).toEqual("2014-04-20");
+            expect(race.circuit.location.country).toEqual("China");
+            done();
+        });
+    }, 10000);
+
+    it("Should return an error because Formula 1 didn't exist in 1900", (done: any) => {
+        ergastClient.getRace(1900, 1, function(err: any, race: any) {
+            expect(err).toBeDefined();
+            expect(race).toBeUndefined();
+            done();
+        });
+    }, 10000);
 });
