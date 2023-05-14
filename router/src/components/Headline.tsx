79156f607dbd0286177ec69da932be990072f727
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-05-14 11:15:50 -0400

    added content

--- router/src/components/Headline.tsx
+++ router/src/components/Headline.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Box, Center, Flex, Heading, Link } from '@chakra-ui/react'
 import { Bruno_Ace_SC } from 'next/font/google';
+import { Text } from '@chakra-ui/react';
  
 const roboto = Bruno_Ace_SC({
   weight: '400',
@@ -8,13 +9,25 @@ const roboto = Bruno_Ace_SC({
 });
 
 export const Headline = ({ title }: { title: string }) => (
-  <Flex position='relative' marginTop='0' justifyContent="center" alignItems="center" height="35vh" bgGradient="linear(to-l, #de6262, #ffb88c)" bgClip="text">
+<div>
+  <Flex position='relative' flexDirection="column" marginTop='15' justifyContent="center" alignItems="center" height="35vh" bgGradient="linear(to-l, #de6262, #ffb88c)" bgClip="text">
     <Heading fontSize="6vw">
-        <div className={roboto.className}>
-            {title}
-        </div>
+      <div className={roboto.className}>
+          {title}
+      </div>
     </Heading>
+
+    <div>
+      <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8} marginTop={19}>
+        <code>
+          <Link href="https://www.npmjs.com/package/f1-api" isExternal>
+            npm install f1-api
+          </Link>
+        </code>
+      </Box>
+    </div>
   </Flex>
+</div>
 )
 
 Headline.defaultProps = {
