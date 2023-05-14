f61221271c3063a8f17da49c22f8e687dc701226
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-05-14 10:12:08 -0400

    formatting

--- router/src/components/StyleColorMode.tsx
+++ router/src/components/StyleColorMode.tsx
@@ -2,18 +2,10 @@ import { Flex, useColorMode, FlexProps } from '@chakra-ui/react'
 
 export const StyleColorMode = (props: FlexProps) => {
   const { colorMode } = useColorMode()
-
-  const bgColor = { light: 'white', dark: 'gray.900' }
-
+  const bgColor = { light: 'white', dark: 'black' }
   const color = { light: 'black', dark: 'white' }
+  
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="flex-start"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
-      {...props}
-    />
+    <Flex direction="column" alignItems="center" justifyContent="flex-start" bg={bgColor[colorMode]} color={color[colorMode]} {...props}/>
   )
 }
